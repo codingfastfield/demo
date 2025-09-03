d40f7ff13b1e1f1ad237f92bfff523f870ec56f4
codingfastfield <ianveeshivo@gmail.com>
2025-09-03 18:26:28 +0300

    jay

---
+++ src/data/firmdata.js
@@ -0,0 +1,146 @@
+export const firms = [
+  {
+    id: 1,
+    firm: "Bak",
+    number:"+254722****07*",
+    email:"Bake*@gmail.com****",
+    address:"PR7C+*, Standard St*, Na*r*i**"
+
+  },
+
+    {
+    id: 2,
+    firm: "Kocey",
+    number:"+254722****07*",
+    email:"Kocey*@gmail.com****",
+     address:"Ka* St, Q…way House**4th …, Ci*y C** Na*r*i**",
+
+  },
+    {
+    id: 3,
+    firm: "Georg",
+    number:"020 20****72**",
+    email:"Georg*@gmail.com****",
+     address:"Standar**d Street** Na*r*i**",
+
+  },
+      {
+    id: 4,
+    firm: "Biam",
+    number:"+254722****07*",
+    email:"Biam*@gmail.com****",
+     address:"Trannati***onal Pl**a*a, Mm Ngina S, Na*i**bi",
+
+  },
+
+    {
+    id: 5,
+    firm: "Kihar",
+    number:"+254722****31*",
+    email:"Kihar*@gmail.com****",
+
+     address:"Internat**ional H**se, ** Ngi*--na **Ro-d, **"
+
+  },
+
+      {
+    id: 6,
+    firm: "kenyat",
+    number:"+254729****94*",
+    email:"kenyat*@gmail.com****",
+     address:"Harambe***e Ave***gen House, ** Flr, Rm---ty C_*obi",
+
+  },
+
+      {
+    id: 7,
+    firm: "abdulha",
+    number:"+254722****07*",
+    email:"abdulha*@gmail.com****",
+     address:"Chai House**-, --th Floor -- St, Nairobi",
+
+  },
+
+     {
+    id: 8,
+    firm: "kiok",
+    number:"+254710 51**++==*",
+    email:"kiok*@gmail.com****",
+     address:"Consolidated ===__-- Bank House***++++++======_____-----",
+
+  },
+
+    {
+    id: 9,
+    firm: "Migo",
+    number:"020 258++--==*",
+    email:"Migo*@gmail.com****",
+     address:"Harambee --++ Co-Operative ***___===--__+==",
+
+  },
+
+     {
+    id: 10,
+    firm: "Aruse",
+    number:"020 2249**__--",
+    email:"Aruse*@gmail.com****",
+     address:"Room ***, *th Floor, Kaunda **, Nairobi***",
+
+  },
+
+      {
+    id: 11,
+    firm: "Agin",
+    number:"+020 34**---*",
+    email:"Agin*@gmail.com****",
+     address:" Ufanisi **, **-- Selassie **, Nairobi",
+
+  },
+
+     {
+    id: 12,
+    firm: "Wesong",
+    number:"020 22***---*",
+    email:"Wesong*@gmail.com****",
+     address:"Hamilton House, *** Floor *** St, Nairobi",
+
+  },
+
+    {
+    id: 13,
+    firm: "Namad",
+    number:"+254711 8***--*",
+    email:"Namad*@gmail.com****",
+     address:"The *** Insurance Company of **",
+
+  },
+
+    {
+    id: 14,
+    firm: "Ongot",
+    number:"+254712**--__",
+    email:"Ongot*@gmail.com****",
+     address:"** House, Aga Khan Walk, N**bi",
+
+  },
+
+   {
+    id: 14,
+    firm: "Ombet",
+    number:"020 2251725",
+    email:"Ombet*@gmail.com****",
+     address:"** House, Standard St, Nairobi",
+
+  },
+
+
+
+
+
+
+
+
+
+
+
+]
